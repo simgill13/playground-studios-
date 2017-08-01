acbc07f0f26997bd33f2fed5ebe3a14bff84c0a4
Sim Gill <simgill89@gmail.com>
2017-07-31 21:55:56 -0700

    starter files

--- server.js
+++ server.js
@@ -6,85 +6,22 @@ const app = express();
 app.use(bodyParser.json());
 const path = require('path');
 const {blogPost} = require('./model');
-const {PORT, DATABASE_URL} = require('./config');
+const {PORT, DATABASE_URL} = require('./secret');
+
+
+
+
 app.use('/',express.static(path.join(__dirname, 'client')));
 
+app.get('/post', (req, res) => {})
 
+app.get('/post/:id', (req, res) => {});
 
-app.get('/post', (req, res) => {
-    blogPost
-        .find()
-        .exec()
-        .then(posts => {
-            res.json(posts)
-      	})
-      	.catch(err => {
-      		res.status(500).json({ message:'server error'});
-      	})
-})
+app.post('/post', (req, res) => {})
 
+app.put('/post/:id', (req, res) => {})
 
-app.get('/post/:id', (req, res) => {
-	blogPost
-	.findById(req.params.id)
-	.exec()
-	.then(posts => {
-		res.json(posts)
-	})
-	.catch(err => {
-		res.status(500).json({error: 'something went bad'})
-	});
-});
-
-
-
-
-app.post('/post', (req, res) => {
-	blogPost
-	.create({
-		name: req.body.name,
-		email: req.body.email,
-		subject: req.body.subject,
-		message: req.body.message
-	})
-	.then(newPost =>{
-		res.status(201).json(newPost)
-	})
-	.catch(err => {
-		console.log(err);
-		res.status(500).json({message:'internal server error'});
-	})
-
-})
-
-app.put('/post/:id', (req, res) => {
-
-	const toUpdate = {};
-    const updateableFields = ['name', 'email', 'subject','message'];
-    updateableFields.forEach(field => {
-    if (field in req.body) {
-      toUpdate[field] = req.body[field];
-  }
-    });
- 	blogPost
- 	.findByIdAndUpdate (req.params.id, {$set: toUpdate})
- 	.exec()
- 	.then(updatedPost => res.status(204).end())
- 	.catch(err => res.status(500).json({message: 'Internal server error'}));
-  })
-
-
-
-
-
-
-app.delete('/post/:id', (req , res) =>{
-	blogPost
-	.findByIdAndRemove(req.params.id)
-	.exec()
-	.then(post => res.status(204).end())
-	.catch(err => res.status(500).json({message: 'Internal server error'}));
-});
+app.delete('/post/:id', (req , res) =>{});
 
 
 
@@ -130,7 +67,7 @@ if (require.main === module) {
   runServer().catch(err => console.error(err));
 };
 
-module.exports = {app, runServer, closeServer};
+
 
 
 
